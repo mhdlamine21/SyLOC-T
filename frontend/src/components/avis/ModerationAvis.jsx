fa51b98a27e2c952b2777981aee64445376496d0
mhdlamine21 <mouhamedlniang@gmail.com>
2026-08-11 11:17:46 +0000

    feat(lamine_work): contains strictly Lamine tickets (LR-2 to LR-7, LR-19 to LR-21)

--- frontend/src/components/avis/ModerationAvis.jsx
+++
@@ -1,8 +0,0 @@
-export default function NomComposant() {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold">Nom du composant</h1>
-      <p>En cours de développement...</p>
-    </div>
-  );
-}
